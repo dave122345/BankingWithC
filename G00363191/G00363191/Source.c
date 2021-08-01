30eaec1bd1a2cfabca5cb1553882c402bb2de0bd
David Nolan <davidnolan458@gmail.com>
2021-08-01 16:09:23 +0100

    update

--- G00363191/G00363191/Source.c
+++ G00363191/G00363191/Source.c
@@ -4,7 +4,7 @@
 #include<string.h>
 #include<math.h>
 
-typedef struct  {
+typedef struct {
 	int accountNum;
 	char Fname[30];
 	char Lname[30];
@@ -14,6 +14,14 @@ typedef struct  {
 	char rating[30];
 }Account;
 
+typedef struct {
+
+	char Username[30];
+	char Password[30];
+
+}Users;
+
+
 
 //defines and functions here
 void print_account(Account* account)
@@ -22,7 +30,7 @@ void print_account(Account* account)
 	{
 		return;
 	}
-	
+
 	printf("Account number: %d\n", account->accountNum);
 	printf("First Name: %s\n", account->Fname);
 	printf("Last Name: %s\n", account->Lname);
@@ -37,42 +45,126 @@ void print_account(Account* account)
 }
 
 
+void load_user(Users* users)
+{
+
+
+
+	printf("\n");
+
+}
+
+
 void main()
 {
 	//variables
-	int choice;
-	Account* account= malloc(sizeof(Account)); 
-	
-	account->accountNum = 000001; 
+	int choice = 0;
+	char user_attempt[30];
+	char pass_attempt[30];
+	char username[30];
+	char password[30];
+	Account* account = malloc(sizeof(Account));
+
+	/*account->accountNum = 000001;
 	strcpy(account->Fname, "Ben");
 	strcpy(account->Lname, "O'Connor");
 	strcpy(account->Address, "Somewhere");
 	account->Balance = 001.00;
 	strcpy(account->Email, "Ben@gmail.com");
-	strcpy(account->rating, "Good");
-	
-	print_account(account);
+	strcpy(account->rating, "Good");*/
+
+	//print_account(account);
 
 	free(account);
 
 	//start
-	
 
-	/*
-	FILE* f = fopen("Savegame.dat", "rb");
-	printf("Loading Game\n");
-	fread(drawn, sizeof(int), 100, f);
-	fread(&Numofplayers, sizeof(int), 1, f);
-	printf("\nNumofplayers = %d\n", Numofplayers);
-	for (int p = 0; p < Numofplayers; p++)
-	{
-		players[p] = malloc(sizeof(bingo_player));
-		fread(players[p], sizeof(struct bingo_player), 1, f);
+	FILE* f = fopen("Users.txt", "r");
+
+	//printf("File open\n");
+
+	Users* userList[3];
+
+
+	int i = 0;
+	while (fscanf(f, "%s %s", username, password) == 2) {
+
+		Users* users = malloc(sizeof(Users));
+		strncpy(users->Username, username, 30);
+		strncpy(users->Password, password, 30);
+		printf("%s %s\n", users->Username, users->Password);
+
+		userList[i] = users;
+		i++;
+
+
+
+
 	}
-	fclose(f);*/
 
-	printf("Menu");
-	scanf("%d", &choice);
+	fclose(f);
+	//printf("file closed \n");
+
+
+	//free(user2);
+
+
+	int a = 1;
+	int u = 0;
+
+	printf("Welcome please enter username and password to log in \n");
+
+
+	while (a == 1) {
+
+		printf("Please enter your username:\n");
+		scanf("%s", user_attempt);
+
+
+
+		for (i = 0; i < 3; i++) {
+			//printf("%s %d \n", userList[i]->Username, i);
+
+			if (strcmp(user_attempt, userList[i]->Username) == 0) {
+
+
+
+				printf("user found!\n");
+				u = 1;
+				//ask for Password
+
+				printf("Please enter your Password:\n");
+				scanf("%s", pass_attempt);
+
+				if (strcmp(pass_attempt, userList[i]->Password) == 0) {
+
+					
+					a = 0;
+					break;
+
+				}
+
+				else {
+					printf("incorrect details try again\n");
+					break;
+
+
+				}
+
+
+
+			}
+
+		}
+		if (u == 0) {
+			printf("incorrect details try again\n");
+			
+		}
+
+
+
+	}
+
 
 	if (choice == 1)
 	{
@@ -92,6 +184,16 @@ void main()
 		exit(0);
 	}
 
+
+
+
+
+	//clean up users
+	for (i = 0; i < 3; i++) {
+		if (userList[i] != NULL) {
+			free(userList[i]);
+		}
+	}
 
 
 
